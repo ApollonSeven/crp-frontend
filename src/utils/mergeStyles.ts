ae593490753398c2a9f45399bad1e5cbac87b526
arseniy.beznozhnik <arseniy.beznozhnik@aliexpress.ru>
2023-03-01 20:36:17 +0300

    refactor: add typings, utils, remove mdx story

---
+++ src/utils/mergeStyles.ts
@@ -0,0 +1,7 @@
+export function mergeStyles<T extends Record<string, unknown>>(defaultStyles: T, styles: T | undefined): T {
+  if (!styles) return defaultStyles;
+  return {
+    ...defaultStyles,
+    ...styles,
+  };
+}
