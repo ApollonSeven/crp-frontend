8cf22b39578410ddafa53d8e5ac07d20a2237aba
Chosensero <91271879+Chosensero@users.noreply.github.com>
2023-03-14 00:15:38 +0100

    fix: reworked design
    Made some imporevements and renamed some styles for better understanding.

--- src/shared/ui/MedCard/MedCard.tsx
+++ src/shared/ui/MedCard/MedCard.tsx
@@ -15,49 +15,55 @@ const MedCard: React.FC<MedCardProps> = ({
 }) => {
     return (
         <div className={styles['container']}>
-            <div className={styles.cardIcon}></div>
-            <div className={styles.rectangle}></div>
-            <div className={styles.rectangle2}></div>
             <div className={styles.names}>
-                <span className={styles.bigText}> {`${corpName}`}</span>
-                <span className={styles.bigRedText}> {`${subName}`}</span>
+                <span className={styles.titleText}> {`${corpName}`}</span>
+                <span className={styles.redTitleText}> {`${subName}`}</span>
             </div>
 
             <div className={styles.date}>
                 {' '}
                 <span>{'Срок действия:'} </span>{' '}
-                <span className={styles.text}>{`${effectiveDate}`} </span>{' '}
+                <span className={styles.dateText}>{`${effectiveDate}`} </span>{' '}
             </div>
             <div className={styles.info}>
-                <span>{'Имя:'} </span>{' '}
-                <span className={styles.text}>
-                    {`${firstName}`} {`${lastName}`}{' '}
-                </span>
-                <span>{'ID:'} </span>{' '}
-                <span className={styles.text}>{`${id}`} </span>
+                <div className={styles.infoBlock}>
+                    {' '}
+                    <span>{'Имя:'} </span>{' '}
+                    <span className={styles.text}>
+                        {`${firstName}`} {`${lastName}`}{' '}
+                    </span>{' '}
+                </div>
+                <div className={styles.infoBlock}>
+                    {' '}
+                    <span>{'ID:'} </span>{' '}
+                    <span className={styles.text}>{`${id}`} </span>{' '}
+                </div>
             </div>
 
             <div className={styles.busCard}>
                 <div className={styles.busCardText}>
-                    <div>
+                    <div className={styles.busBlock}>
                         {' '}
                         <span>{'Получено в:'} </span>{' '}
                         <span className={styles.text}>
                             {`${givenAddress}`}{' '}
                         </span>{' '}
                     </div>
-                    <div>
+                    <div className={styles.busBlock}>
                         {' '}
                         <span>{'Телефон:'} </span>{' '}
                         <span className={styles.text}>{`${phoneNumber}`} </span>{' '}
                     </div>
-                    <div>
+                    <div className={styles.busBlock}>
                         {' '}
                         <span>{'Медицинская группа:'} </span>{' '}
                         <span className={styles.text}>{`${medGroup}`} </span>{' '}
                     </div>
                 </div>
             </div>
+            <div className={styles.cardIcon}></div>
+            <div className={styles.rectangle}></div>
+            <div className={styles.rectangle2}></div>
         </div>
     )
 }
