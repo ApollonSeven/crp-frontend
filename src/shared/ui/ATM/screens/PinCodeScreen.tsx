33c80367a28e7708a0a26c0a543d3bca9d16bc54
Maksim <mex83011@gmail.com>
2023-05-04 17:55:59 +0300

    refactor: pinCodeScreen component

--- src/shared/ui/ATM/screens/PinCodeScreen.tsx
+++ src/shared/ui/ATM/screens/PinCodeScreen.tsx
@@ -1,24 +1,55 @@
 import React, {useState} from 'react'
 import styles from './PinCodeScreen.module.scss'
-import classNames from 'classnames'
 
 const PinCodeScreen = () =>{
 
-    const [isError] = useState(true)
+    const [pin, setPin] = useState('')
+    const [pinMask, setPinMask] = useState(pin)
+    const [isError, setIsError] = useState(false)
+    
+    const sendPin = (pin:string) => {
+        setIsError(pin == '0000' ? false : true) //тут будет отправка на бэкэнд
+        setPin('')
+    }
+
+    const getPinMask = (pin:string) => {
+        setPinMask(pin + '\u00b7'.repeat(4 - pin.length))
+    }
+
+    const getCharValidation = (char:string) => {
+        const regexp = /[0-9]/g
+        return regexp.test(char)
+    }
+
+    const handlePinChange = (event:any) => {
+        if (getCharValidation(event.target.value[event.target.value.length - 1])) {
+            setPin(pin + event.target.value[event.target.value.length - 1])
+            getPinMask(pin)
+            pin.length == 4 && sendPin(pin)
+        } else {
+            setPin('')
+            getPinMask
+        }
+    }
 
     return(
         <div className={styles.cardShape}>
-            <div id = 'pinCodeField' className={styles.pinCodeField}>
-                <div id='firstLetter' className={classNames(styles.pinCodeFieldChar, styles.pinChar)}>1</div>
-                <div className={classNames(styles.pinCodeFieldChar, styles.pinShape)}></div>
-                <div className={classNames(styles.pinCodeFieldChar, styles.pinShape)}></div>
-                <div className={classNames(styles.pinCodeFieldChar, styles.pinShape)}></div>
-            </div>
+            <input
+                id='input'
+                className={styles.pinCodeInput}
+                autoFocus
+                type='text'
+                maxLength={4}
+                placeholder='&#183;&#183;&#183;&#183;'//\u00B7\u00B7\u00B7\u00B7
+                onChange ={handlePinChange}
+                value={pinMask}
+                />
             <div className={(isError? `${styles.screenAlertError}` : `${styles.screenAlert}`)}>
                 <div className={(isError? `${styles.screenAlertTextError}` : `${styles.screenAlertText}`)}>{isError? 'Пароль введен неверно' : 'Для отказа операции нажмите кнопку "Завершить обслуживание'}</div>
             </div>
         </div>
     )
 }
+
 
 export default PinCodeScreen;
